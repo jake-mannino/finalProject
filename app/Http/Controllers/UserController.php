55be178fad438b60ed723ec4e04368100ad6533f
yomannino <yomannino@gmail.com>
2021-05-03 18:17:10 -0400

    gen more user tables, working on valid() faker factory data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -22,9 +25,31 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'username' => 'required|string|starts_with:OG,O.G.,drPhill,Dr. Phill,DrPhill,lil,bigHomie,BigHomie,yung,Yung,Dr,Dr.,dr,Professor,professor,Sir,sir,Lord,lord,Shrek,shrek,getOuttaMySwamp|unique:App\Models\User,username',
+            'email' => 'nullable|email|max:64|unique:App\Models|User,email_address',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['message' => 'hella validation errors. so just throw the whole computer away idk come back next year or something..', 'errors' =>  $validator->errors(), 'status' => false], 422);
+        }
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+
+        //Creating new user
+        $user = User::create($input);
+
+
+        /**Take note of this: Your user authentication access token is generated here **/
+        $data['token'] =  $user->createToken('AudioTornado')->accessToken;
+        $data['user_data'] = $user;
+
+        return response(['data' => $data, 'message' => 'Digital Qubit clone of your consciousess created successfully! Login asap or else your clone will be *repurposed*..', 'status' => true]);
     }
 
     /**
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,14 +22,32 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        //    $usernameValidator = function($userName) {
+        //        $string = $userName;
+        //        function startsWith ($string, $startString)
+        //        {
+        //        $len = strlen($startString);
+        //        return (substr(startsWith ($string, 0, $len) === $startString);
+        //        }
+        //        return bool
+
+        //        //have to bind call or this but laravel version to get scope right for validating faker data
+        //    }
+        // 'name' => 'required|string',
+        // 'username' => 'required|string|starts_with:OG,O.G.,drPhill,Dr. Phill,DrPhill,lil,bigHomie,BigHomie,yung,Yung,Dr,Dr.,dr,Professor,professor,Sir,sir,Lord,lord,Shrek,shrek,getOuttaMySwamp|unique:App\Models\User,username',
+        // 'email' => 'nullable|email|max:64|unique:App\Models|User,email_address',
+        // 'password' => 'required|string|min:8',
+
+        // ]);
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->unique()->name,
+            //valid()->
             'email' => $this->faker->unique()->safeEmail,
-            'password' => 'changeme', // password
+            //valid()->
+            'password' => 'richHomieQuan', // password
             'username' => $this->faker->unique()->userName,
+            //valid()->
             // 'remember_token' => Str::random(10),
-
-
             'profile_pic' => '',
             'cover_pic' => '',
             'favorites' => 0,
