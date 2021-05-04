ecbccc6d242d015c0a9a3810100da85a1bd13312
yomannino <yomannino@gmail.com>
2021-05-04 10:34:19 -0400

    finished first story on laravel user register for backend, tried to do weird facotry validation method referencing usercontroller validation but only works for actual users signing up and not the faker.js in the factory, this was done last night, except i had to comment out my personal acces token line in the authserviceprovider line 38, was throwing an error in postman when I was trying to register myself as a user and add my table coloum to the faker.js users seeded from my userfactory and migrated into phpmyadmin, everything worked after commenting this out today

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -35,6 +35,7 @@ class AuthServiceProvider extends ServiceProvider
             Passport::tokensExpireIn(now()->addDays(15));
             Passport::refreshTokensExpireIn(now()->addDays(30));
             Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+            Passport::personalAccessClientId('client-id');
         }
 
 
